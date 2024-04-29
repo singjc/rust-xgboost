0307a92960290bbe74b69cdeff2dd9090201f346
Montana Low <montanalow@gmail.com>
2024-04-29 11:54:50 -0700

    bump version, fix c++ stdlib bindgen (un)support

--- xgboost-sys/build.rs
+++ xgboost-sys/build.rs
@@ -50,6 +50,7 @@ fn main() {
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
+        .blocklist_item("std::.*")// stdlib is not well supported by bindgen
         .clang_args(&["-x", "c++", "-std=c++11"])
         .clang_arg(format!("-I{}", xgb_root.join("include").display()))
         .clang_arg(format!("-I{}", xgb_root.join("rabit/include").display()))
