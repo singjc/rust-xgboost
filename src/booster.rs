98604a006d8959f9c605deb122c0beb507dfd5c8
Montana Low <montanalow@gmail.com>
2025-01-28 17:52:49 -0800

    rabit checkpoints removed upstream

--- src/booster.rs
+++ src/booster.rs
@@ -148,29 +148,8 @@ impl Booster {
             dmats
         };
 
-        let mut bst = Booster::new_with_cached_dmats(&params.booster_params, &cached_dmats)?;
-        // load distributed code checkpoint from rabit
-        let mut version = bst.load_rabit_checkpoint()?;
-        debug!("Loaded Rabit checkpoint: version={}", version);
-        assert!(unsafe { xgboost_sys::RabitGetWorldSize() != 1 || version == 0 });
-        let start_iteration = version / 2;
-        for i in start_iteration..params.boost_rounds as i32 {
-            // distributed code: need to resume to this point
-            // skip first update if a recovery step
-            if version % 2 == 0 {
-                if let Some(objective_fn) = params.custom_objective_fn {
-                    debug!("Boosting in round: {}", i);
-                    bst.update_custom(params.dtrain, objective_fn)?;
-                } else {
-                    debug!("Updating in round: {}", i);
-                    bst.update(params.dtrain, i)?;
-                }
-                let _ = bst.save_rabit_checkpoint()?;
-                version += 1;
-            }
-
-            assert!(unsafe { xgboost_sys::RabitGetWorldSize() == 1 || version == xgboost_sys::RabitVersionNumber() });
-
+        let bst = Booster::new_with_cached_dmats(&params.booster_params, &cached_dmats)?;
+        for i in 0..params.boost_rounds as i32 {
             if let Some(eval_sets) = params.evaluation_sets {
                 let mut dmat_eval_results = bst.eval_set(eval_sets, i)?;
 
@@ -203,10 +182,6 @@ impl Booster {
                 }
                 println!();
             }
-
-            // do checkpoint after evaluation, in case evaluation also updates booster.
-            let _ = bst.save_rabit_checkpoint();
-            version += 1;
         }
 
         Ok(bst)
@@ -566,16 +541,6 @@ impl Booster {
         } else {
             Ok(String::new())
         }
-    }
-
-    pub(crate) fn load_rabit_checkpoint(&self) -> XGBResult<i32> {
-        let mut version = 0;
-        xgb_call!(xgboost_sys::XGBoosterLoadRabitCheckpoint(self.handle, &mut version))?;
-        Ok(version)
-    }
-
-    pub(crate) fn save_rabit_checkpoint(&self) -> XGBResult<()> {
-        xgb_call!(xgboost_sys::XGBoosterSaveRabitCheckpoint(self.handle))
     }
 
     pub fn set_param(&mut self, name: &str, value: &str) -> XGBResult<()> {
